ea7e5aaa71e71bc19e55b58a8794eeaf62285d40
Jamie <bkx18uyu@uea.ac.uk>
2021-10-06 21:18:22 +0100

    Initial lexer framework
    Initial development of the Lexer

---
+++ Tokenizer/Lexer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tokenizer
+{
+    public class Lexer
+    {
+        public List<Token> Tokens;
+
+        private Dictionary<String, TokenType> _dict = new KeyWords().Dict;
+
+        public Lexer(String inStream) // Pass input stream/file path
+        {
+            /*
+             Go char by char, order of play:
+             skip white spaces other than tabs, 
+             single character tokens,
+             strings (double/single quote),
+             everything else  
+             
+             Whenever hit identifier check if its a key word           
+             */
+            Tokens = new List<Token>();
+
+            String temp = "";
+            TokenType result;
+
+            foreach (char c in inStream)
+            {
+                if (c == '?' || c == '!')
+                {
+                    Tokens.Add(new Token(TokenType.Question, temp));
+                    temp = "";
+                }
+                else if (Char.IsWhiteSpace(c))
+                {
+                    //TODO Put in optimised order i.e. size order
+                    switch (temp)
+                    {
+                        case "EOF":
+                            Tokens.Add(new Token(TokenType.EoF, ""));
+                            break;
+                        case "can":
+                            Tokens.Add(new Token(TokenType.Can, temp));
+                            break;
+                        case "you":
+                            Tokens.Add(new Token(TokenType.You, temp));
+                            break;
+                        case "bench":
+                            Tokens.Add(new Token(TokenType.Bench, temp));
+                            break;
+                        case "plates":
+                            Tokens.Add(new Token(TokenType.Plates, temp));
+                            break;
+                        default:
+                            if (IsDigit(temp))
+                            {
+                                Tokens.Add(new Token(TokenType.Int, temp));
+                            }
+                            else if (IsString(temp))
+                            {
+                                Tokens.Add(new Token(TokenType.Str, temp));
+                            }
+                            else if (_dict.TryGetValue(temp, out result))
+                            {
+                                Tokens.Add(new Token(result, temp));
+                            }
+                            else if (IsVar(temp))
+                            {
+                                Tokens.Add(new Token(TokenType.Var, temp));
+                            }
+                            else
+                            {
+                                Tokens.Add(new Token(TokenType.Illegal, temp));
+                            }
+                            break;
+                    }
+                    temp = "";
+                }
+                else
+                {
+                    temp += c;
+                }
+            }
+        }
+
+        private static bool IsDigit(String input)
+        {
+            foreach (char c in input)
+            {
+                if ('0' <= c && c <= '9')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        private static bool IsString(String input)
+        {
+            if (input[0] == '\"' && input[input.Length] == '\"')
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        private static bool IsVar(String input)
+        {
+            foreach (char c in input)
+            {
+                if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Tokenizer/Tokens.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tokenizer
+{
+    // Enum of all token types for the language
+    public enum TokenType
+    {
+        Illegal,    // Illegal character
+        EoF,        // End of file
+        
+        Int,        // Integer
+        Str,        // String
+        Var,        // Variable
+        
+        Bench,      // int
+
+        Question,   // "?" == ";"
+        Can,        // Equals 
+        You,        // Equals
+        Plates     // Integer
+    }
+
+    public class KeyWords
+    {
+        public Dictionary<String, TokenType> Dict = new Dictionary<String, TokenType>()
+        {
+            //{TokenType.If, "if"}
+            //{TokenType.For, "for"},
+            //{TokenType.While, "while"}
+        };
+    }
+
+    public class Token
+    {
+        public TokenType type;
+        public string contents; // int x = 10
+
+        public Token(TokenType type, string contents)
+        {
+            this.type = type;
+            this.contents = contents;
+        }
+
+        public override string ToString()
+        {
+            return this.type.ToString();
+        }
+    }
+
+    public class Tokenizer
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Lexer x = new Lexer("can you bench 2 plates x?");
+            x.Tokens.ForEach(Console.WriteLine);
+        } 
+    }
+}
