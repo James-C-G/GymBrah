8fd7aea250a6fa56e4d88c32ec01fc0f52f40a13
Jamie <bkx18uyu@uea.ac.uk>
2021-11-15 01:33:22 +0000

    Initial Development of Top Down Parser
    Top down parser initial development for a basic calculator. Integration of previous lexer and tokenisation code.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        private int _counter;
+        private TokenType _currentToken;
+        public List<Token> Tokens;
+        
+        public Calculator(List<Token> tokens)
+        {
+            Tokens = tokens;
+            _currentToken = tokens[_counter].Type;
+        }
+        
+        private Token ScanToken(List<Token> tokens)
+        {
+            Token token = tokens[_counter];  
+            _counter++; 
+            _currentToken = tokens[_counter].Type; 
+            return token;
+        }
+        
+        private Node Factor()
+        {
+            switch (_currentToken)
+            {
+                case TokenType.Integer:
+                {
+                    return new Integer(ScanToken(Tokens).IntegerContent);
+                }
+                case TokenType.OpenBrace:
+                {
+                    ScanToken(Tokens);
+                    Node node = Expression();
+                    
+                    if(_currentToken == TokenType.CloseBrace)
+                    {
+                        ScanToken(Tokens);
+                        return node;
+                    }
+
+                    return null;
+                }
+                default:
+                {
+                    return null;
+                }
+            }
+        }
+        
+        private Node Term()
+        {
+            Node nodeOne = Factor();
+
+            while (true)
+            {
+                switch (_currentToken)
+                {
+                    case TokenType.Multiply:
+                    {
+                        ScanToken(Tokens);
+                        Node nodeTwo = Factor();
+                        
+                        nodeOne = new Multiply(nodeOne, nodeTwo);
+                        break;
+                    }
+                    case TokenType.Divide:
+                    {
+                        ScanToken(Tokens);
+                        Node nodeTwo = Factor();
+                        
+                        nodeOne = new Divide(nodeOne, nodeTwo);
+
+                        break;
+                    }
+                    default:
+                    {
+                        return nodeOne;
+                    }
+                }
+            }
+        }
+        
+        public Node Expression()
+        {
+            Node nodeOne = Term();
+            
+            while (true)
+            {
+                switch (_currentToken)
+                {
+                    case TokenType.Addition:
+                    {
+                        ScanToken(Tokens);
+                        Node nodeTwo = Term();
+                    
+                        nodeOne = new Add(nodeOne, nodeTwo);
+
+                        break;
+                    }
+                    case TokenType.Subtraction:
+                    {
+                        ScanToken(Tokens);
+                        Node nodeTwo = Term();
+                    
+                        nodeOne = new Subtract(nodeOne, nodeTwo);
+
+                        break;
+                    }
+                    default:
+                    {
+                        return nodeOne;
+                    }
+                }
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lexer lex = new Lexer("3 * 5 * 3?");
+
+            Calculator calculator = new Calculator(lex.Tokens);
+            Node result = calculator.Expression();
+            int e = result.Evaluate();
+            Console.Out.WriteLine(e);
+        }
+    }
+}
---
+++ Calculator/Node.cs
@@ -0,0 +1,88 @@
+﻿namespace Calculator
+{
+    public abstract class Node
+    {
+        public Token NodeToken;
+        public abstract int Evaluate();
+    }
+    
+    public class Integer : Node
+    {
+        public new Token NodeToken;
+        public Integer(int value)
+        {
+            NodeToken = new Token(TokenType.Integer, value);
+        }
+        public override int Evaluate()
+        {
+            return NodeToken.IntegerContent;
+        }
+    }
+    public class Add : Node
+    {
+        public Node Left, Right;
+        public Add(Node left, Node right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public override int Evaluate()
+        {
+            return Left.Evaluate() + Right.Evaluate();
+        }
+
+    }
+    public class Subtract : Node
+    {
+        public Node Left, Right;
+        public Subtract(Node left, Node right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public override int Evaluate()
+        {
+            return Left.Evaluate() + Right.Evaluate();
+        }
+    }
+    public class Multiply : Node
+    {
+        Node Left, Right;
+        public Multiply(Node left, Node right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public override int Evaluate()
+        {
+            return Left.Evaluate() * Right.Evaluate();
+        }
+    }
+    public class Divide : Node
+    {
+        public Node Left, Right;
+        public Divide(Node left, Node right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public override int Evaluate()
+        {
+            return Left.Evaluate() / Right.Evaluate();
+        }
+    }
+    
+    public class Negate : Node
+    {
+        public Node Value;
+        
+        public Negate(Node value)
+        {
+            Value = value;
+        }
+        public override int Evaluate()
+        {
+            return -Value.Evaluate();
+        }
+    }
+}
